b6dd0cc9de9eeac69cceabb9d02e69c8a577990e
Adam Orban <38665307+cafger83@users.noreply.github.com>
2018-04-23 21:49:22 +0200

    Add files via upload

--- BinComLib/BinComLib.cpp
+++ BinComLib/BinComLib.cpp
@@ -27,6 +27,7 @@ volatile bool _bejovo_ready = false;  //van-e ervenyes adat a bejovo pufferben
 byte _adat_hossz; // hany bajttal kell majd dolgozni (1..8)
 byte _kezdocim;   // adattomb hanyadik eleme (elso elem: 0)
 bool _ir_olvas;   // true=iras, false=olvasas
+byte _crc;		  // crc szamitashoz
 
 // soros kommunikacio valtozoi a kuldesben
 bool _kimeno_ready = true; //true, ha ures a kuldo puffer, false, ha folyamatban van a kuldes
@@ -36,8 +37,6 @@ byte _kimeno_mutato = 255;
 
 BinComLib::BinComLib(){};
 
-// konyvtar inicializalas
-// allomas_szam: 1..7 kozott kell legyen
 void BinComLib::init(byte _allomas_szam)
 {
   // elforgatott allomasszam
@@ -51,80 +50,100 @@ void BinComLib::init(byte _allomas_szam)
   UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
   // tx, rx enabled, rx interrupt enabled
   UCSR0B = ((1 << TXEN0) | (1 << RXEN0) | (1 << RXCIE0));
+  
 }
 
-// bejovo adatfeldolgozas, minden ciklusban meg kell hivni
-void BinComLib::process(byte *_olvashato_adattomb, byte _olvashato_hossz, byte *_irhato_adattomb, byte _irhato_hossz)
+int BinComLib::process(byte *_regiszter_tomb, byte _regiszter_hossz, byte *_parancs_tomb)
 {
   //megnezzuk, erkezett-e adatcsomag, es valaszt tudunk-e kuldeni, ha igen: mehet a feldolgozas
   if ((_bejovo_ready) && (_kimeno_ready))
   {
     //alapadatok kibanyaszasa
     if ((_bejovo_adatok[0] & B00001000) != 0) _ir_olvas = true; else _ir_olvas = false;
-    _adat_hossz = (_bejovo_adatok[0] & B00000111) + 1;
-    _kezdocim = ((_bejovo_adatok[1] & B00001111) << 4) + (_bejovo_adatok[2] & B00001111);
-
-    //ha irasrol van szo, akkor az irhato tombbe az adatokat be kell paszirozni
+	_adat_hossz = (_bejovo_adatok[0] & B00000111) + 1;
+	
+    //ha parancs irasrol van szo, akkor az irhato tombbe az adatokat be kell paszirozni
     if (_ir_olvas)
     {
-      // tombmeret tullepes ellenorzese
-      int _vegso_meret = (int)_kezdocim + (int)_adat_hossz;
-      if (_vegso_meret > _irhato_hossz)
-      {
-        //eldobjuk, a master majd timeout-ra fut...
-        _bejovo_ready = false;
-        return;
-      }
 
-      // feldolgozas: az adatokat beirjuk a tombbe
+	  // feldolgozas: az adatokat beirjuk a tombbe
       for (byte _fut_f = 0; _fut_f < _adat_hossz; _fut_f++)
-        _irhato_adattomb[_kezdocim + _fut_f] = ((_bejovo_adatok[(_fut_f << 1) + 3] & B00001111) << 4) + (_bejovo_adatok[(_fut_f << 1) + 4] & B00001111);
+        _parancs_tomb[_fut_f] = ((_bejovo_adatok[(_fut_f << 1) + 1] & B00001111) << 4) + (_bejovo_adatok[(_fut_f << 1) + 2] & B00001111);
 
-      // valasz kuldese
-      _kimeno_adatok[0] = _bejovo_adatok[0] & B10001111; //ugyanaz mint ami erkezett, csak a celallomas cime 0;
-      _kimeno_adatok[1] = _bejovo_adatok[1] & B00001111;
-      _kimeno_adatok[2] = _bejovo_adatok[2] & B00001111;
-      _kimeno_hossz = 3;
-      _kimeno_mutato = 0;
+	  //CRC check kell meg ide!
+	  
+	  return _adat_hossz;
 
     } else
 
-      //ha olvasasrol van szo, akkor az olvashato tombbol kinyomjuk az adatokat
+      //ha regiszter olvasasrol van szo, akkor az olvashato tombbol kinyomjuk az adatokat
     {
-      // tombmeret tullepes ellenorzese
+		
+	  _kezdocim = ((_bejovo_adatok[1] & B00001111) << 4) + (_bejovo_adatok[2] & B00001111);
+	  
+	  // tombmeret tullepes ellenorzese
       int _vegso_meret = (int)_kezdocim + (int)_adat_hossz;
-      if (_vegso_meret > _olvashato_hossz)
+      if (_vegso_meret > _regiszter_hossz)
       {
         //eldobjuk, a master majd timeout-ra fut...
         _bejovo_ready = false;
-        return;
+        return 0;
       }
 
-      // feldolgozas
-      // nincs mit feldolgozni, csak a valaszt kell kuldeni
+      //CRC check kell meg ide!
 
       // valasz kuldese
       _kimeno_adatok[0] = _bejovo_adatok[0] & B10001111; //ugyanaz mint ami erkezett, csak a celallomas cime 0;
-      _kimeno_adatok[1] = _bejovo_adatok[1] & B00001111;
-      _kimeno_adatok[2] = _bejovo_adatok[2] & B00001111;
       for (byte _fut_f = 0; _fut_f < _adat_hossz; _fut_f++)
       {
-        _kimeno_adatok[(_fut_f << 1) + 3] = _olvashato_adattomb[_kezdocim + _fut_f] >> 4;
-        _kimeno_adatok[(_fut_f << 1) + 4] = _olvashato_adattomb[_kezdocim + _fut_f] & B00001111;
+        _kimeno_adatok[(_fut_f << 1) + 1] = _regiszter_tomb[_kezdocim + _fut_f] >> 4;
+        _kimeno_adatok[(_fut_f << 1) + 2] = _regiszter_tomb[_kezdocim + _fut_f] & B00001111;
       }
-      _kimeno_hossz = 3 + (_adat_hossz << 1);
-      _kimeno_mutato = 0;
+	  _kimeno_hossz = 3 + (_adat_hossz << 1);
+	  _kimeno_mutato = 0;
+	  
+	  // kamu crc szamitas
+	  _kimeno_adatok[_kimeno_hossz-2] = B00001111; //kamu crc ertek
+	  _kimeno_adatok[_kimeno_hossz-1] = B00001111; //kamu crc ertek
+	  
+	  _kimeno_ready = false;
+	  // RS485 EN bekapcsolasa
+	  
     }
 
     _bejovo_ready = false;
-    _kimeno_ready = false;
-    // RS485 EN bekapcsolasa
   }
 
-  return;
+  return 0;
 }
 
-// kimenp adatkuldes, minden ciklusban meg kell hivni
+void BinComLib::reply(byte *_parancs_tomb, byte _parancs_hossz)
+{
+	if (_kimeno_ready)
+	{
+	  // fejlec generalasa
+	  _kimeno_adatok[0] = B10001000 + (_parancs_hossz - 1);
+	  
+	  // adat darabolas es atmasolas
+      for (byte _fut_f = 0; _fut_f < _parancs_hossz; _fut_f++)
+      {
+        _kimeno_adatok[(_fut_f << 1) + 1] = _parancs_tomb[_fut_f] >> 4;
+        _kimeno_adatok[(_fut_f << 1) + 2] = _parancs_tomb[_fut_f] & B00001111;
+      }
+	  
+	  // kuldesi parameterek atadasa
+	  _kimeno_hossz = 3 + (_parancs_hossz << 1);
+	  _kimeno_mutato = 0;
+	  
+	  // kamu crc szamitas
+	  _kimeno_adatok[_kimeno_hossz-2] = B00001111; //kamu crc ertek
+	  _kimeno_adatok[_kimeno_hossz-1] = B00001111; //kamu crc ertek
+	  
+	  _kimeno_ready = false;
+	  // RS485 EN bekapcsolasa
+	}
+}
+
 void BinComLib::send()
 {
   // ha van mit kuldeni...
@@ -142,7 +161,6 @@ void BinComLib::send()
   return;
 }
 
-// soros port adaterkezes interrupt
 SIGNAL (USART_RX_vect)
 {
   //bejovo byte beolvasasa
@@ -158,12 +176,12 @@ SIGNAL (USART_RX_vect)
     _bejovo_adatok[0] = _bejovo_adat;
     if ((_bejovo_adat & B00001000) != 0)
     {
-      // irast ker a master
-      _bejovo_hossz = (2 * ((_bejovo_adat & B00000111) + 1)) + 2;
+      //parancs irast ker a master, jon 2*x bajt parameter, 2 bajt parancs es 2 bajt CRC
+      _bejovo_hossz = (2 * (_bejovo_adat & B00000111)) + 4;
     } else
     {
-      //olvasast ker a master, meg ket bajt fog jonni a cimmel
-      _bejovo_hossz = 2;
+      //regiszter olvasast ker a master, meg ket bajt fog jonni a cimmel es ketto CRC
+      _bejovo_hossz = 4;
     }
     _bejovo_mutato = 0;
     return;
@@ -184,6 +202,12 @@ SIGNAL (USART_RX_vect)
   }
 
   return;
+}
+
+void debug_send(byte adatom)
+{
+  while ((UCSR0A & 32) == 0);
+  UDR0 = adatom;
 }
 
 
--- BinComLib/BinComLib.h
+++ BinComLib/BinComLib.h
@@ -3,6 +3,7 @@
   BinComLib.h
   
   Binaris kommunikacio PC es Arduino kozott
+  Fixen bedrotozva 0-as sorosport, 115200 baud, 8N1
   
   by Adam Orban, 2018
   
@@ -24,11 +25,23 @@ class BinComLib
 	void init(byte _allomas_szam);
 
 	// bejovo adatfeldolgozas, minden ciklusban meg kell hivni
-	void process(byte *_olvashato_adattomb, byte _olvashato_hossz, byte *_irhato_adattomb, byte _irhato_hossz);
+	// regiszter tomb: ebben vannak a csak olvashato mezok
+	// regiszter hossz: a regiszter tomb merete bajtban
+	// parancs tomb: a fogadott parancsokat ebbe teszi a fuggveny, merete: 8 bajt
+	// visszateresi ertek: ha nem erkezett parancs, akkor 0, ha erkezett parancs,
+	// akkor a parancs tombben atadott bajtok szama (1..8)
+	int process(byte *_regiszter_tomb, byte _regiszter_hossz, byte *_parancs_tomb);
 
-	// kimenp adatkuldes, minden ciklusban meg kell hivni
+	// kimeno adatfeldolgozas, akkor kell meghivni, ha egy parancsot meg akarunk valaszolni
+	// parancs_tomb: a kuldendo parancsot tartalmazza, merete 8 bajt
+	// parancs hossz: a kuldendo tombben levo hasznos bajtok szamat tartalmazza (1..8)
+	void reply(byte *_parancs_tomb, byte _parancs_hossz);
+	
+	// kimeno adatkuldes, minden ciklusban meg kell hivni
 	void send();
 };
+
+void debug_send(byte adatom);
 
 #endif
 
